4c1117f61355d50f8e83be3adab61a535c4ad0f3
publictona <landgesushma2@gmail.com>
2022-04-22 06:55:56 +0530

    hi

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
+const userController= require("../controllers/userController")
 const orderController = require("../controllers/orderController")
 const {headerCheck} = require("../middleware/middleware")
 
-//Developer-Batch route handlers
-router.post("/users", headerCheck, orderController.createUser)
-router.post("/products", orderController.createProduct)
-router.post("/orders", headerCheck, orderController.createOrder)
+
+
 
 module.exports = router;
 
